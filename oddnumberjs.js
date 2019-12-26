42e51ae130a859d315cba98e9aa36ba261e2a4a8
Narayanaswamy challa <36071265+nschalla@users.noreply.github.com>
2019-12-26 16:00:00 +0530

    Add files via upload

---
+++ oddnumberjs.js
@@ -0,0 +1,13 @@
+a=prompt("enter the length of array")
+n=[]
+for(k=0;k<a;k++)
+{
+n.push(prompt("enter the value in to array"))
+}
+
+for (i =0; i< n.length; i++)
+	{ 
+    if(n[i]%2!=0)
+		{ 
+        alert(n[i]);
+        }
