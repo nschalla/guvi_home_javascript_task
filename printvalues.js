061139d61e4875be799c2cc2ee449d1db391e66e
Narayanaswamy challa <36071265+nschalla@users.noreply.github.com>
2019-12-26 15:08:49 +0530

    Add files via upload

---
+++ printvalues.js
@@ -0,0 +1,5 @@
+ var obj = {name : "RajiniKanth", age : 33, hasPets : false};
+            function printAllValues(obj) {
+                return Object.values(obj);
+                }
+                    alert(printAllValues(obj));
